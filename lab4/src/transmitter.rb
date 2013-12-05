d579c4d282e511e01cfee6cf7ed86b8804429478
mr-n1ck <nikita.k312@ya.ru>
2013-12-05 11:15:12 +0300

    lab4 fix counters

--- lab4/src/transmitter.rb
+++ lab4/src/transmitter.rb
@@ -6,6 +6,7 @@ module Transmitter
   def recive(file, socket)
     pack_counter = 0
     oob_counter = 0
+    data_counter = 0
 
     loop do
       reader, _, urgent = IO.select([socket], nil, [socket], 10)
@@ -14,17 +15,18 @@ module Transmitter
       if(io = urgent[0])
         io.recv(1, IO::MSG_OOB)
         oob_counter += 1
-        #puts " recive oob #{oob_counter}: #{oob_data}"
+        puts "data: #{data_counter}"
       end
 
       if io = reader[0]
-        package = io.recv(1024)
+        package = io.recv(2048)
         break if (package.nil? || package.empty?)
         file.write(package)
+        data_counter += package.length
         pack_counter += 1
       end
     end
-
+    puts "was recived #{data_counter} data"
     puts "was recived #{oob_counter} oob"
   end
 
@@ -32,26 +34,29 @@ module Transmitter
   def send(file, socket)
     pack_counter = 0
     oob_counter = 0
+    data_counter = 0
 
     loop do
       _, writer = IO.select(nil,[socket], nil, 10)
       break unless writer
       
       if io = writer[0]
-        package = file.read(1024)
+        package = file.read(2048)
         break if (package.nil? || package.empty?)
         io.send(package, 0)
+        data_counter += package.length
         pack_counter += 1
-        #puts "send packs: #{pack_counter}"
 
         if(pack_counter % 200 == 0)
-          #puts "send oob #{oob_counter}: *"
+          #puts "send: #{data_counter}"
           oob_counter += 1
           io.send('*', IO::MSG_OOB)
+          sleep(0.05)
         end
       end
     end
 
+    puts "was send #{data_counter} data"
     puts "was send #{oob_counter} oob"
   end
   
